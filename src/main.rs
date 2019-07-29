e2567bc2cd35b83481bb3f600237f1867909bccd
朱方圆 <zhufangyuan.moon@bytedance.com>
2019-07-29 20:03:17 +0800

    减法

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 #[derive(Debug)]
 struct Vector<T> where T:Copy+Clone{
     value:Vec<T>
 }
 
-impl <T> Vector<T> where T:Add<T,Output=T>+Copy+Clone{
-    fn new_from_list(list:&[T])->Vector<T>{
-        Vector{
-            value:Vec::from(list)
-        }
-    }
+impl <T> Vector<T> where T:Add<T,Output=T>+Sub<T,Output = T>+Copy+Clone{
     fn new(list:Vec<T>)->Vector<T>{
         Vector{
             value:list
@@ -19,19 +14,15 @@ impl <T> Vector<T> where T:Add<T,Output=T>+Copy+Clone{
     fn size(&self)->u32{
         self.value.len() as u32
     }
-    fn insert(&mut self,val:T)->&mut Vector<T>{
-        self.value.push(val)
-        &self
-    }
+
 }
 //向量加法
-impl <T> Add<Vector<T>> for Vector<T> where T:Add<T,Output=T>+Copy+Clone {
+impl <T> Add<Vector<T>> for Vector<T> where T:Add<T,Output=T>+Sub<T,Output = T>+Clone +Copy {
     type Output = Vector<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
         assert_eq!(self.size() , rhs.size());
         let mut r = Vec::new();
-        let other_iterator = rhs.value.iter();
 
         let size = self.size();
         for i in 0..size{
@@ -40,11 +31,25 @@ impl <T> Add<Vector<T>> for Vector<T> where T:Add<T,Output=T>+Copy+Clone {
         Vector::new(r)
     }
 }
+//向量减法
+impl <T> Sub<Vector<T>> for Vector<T> where T:Add<T,Output=T>+Sub<T,Output = T>+Clone +Copy {
+    type Output = Vector<T>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        assert_eq!(self.size(),rhs.size());
+        let mut result = Vec::new();
+        let size = self.size();
+        for i in 0..size{
+            result.push(self.value[i as usize] - rhs.value[i as usize]);
+        }
+        Vector::new(result)
+    }
+}
 fn main(){
 
-    let v1 = Vector::new(vec![1,2,3,4,5]);
+    let v1 = Vector::new(vec![1,2,3,4,655]);
     let v2 = Vector::new(vec![2,3,4,5,6]);
 
-    println!("{:?}",v1 + v2)
+    println!("{:?}",v1 - v2);
 
 }
