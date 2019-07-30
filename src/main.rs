a972024c629477cacd53c1b2fde3e3cfad960dee
朱方圆 <zhufangyuan.moon@bytedance.com>
2019-07-30 21:59:49 +0800

    格式化输出

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::ops::{Add, Sub};
+use std::fmt::{Display, Formatter,Error};
+
+
 
 #[derive(Debug)]
 struct Vector<T> where T:Copy+Clone{
@@ -45,11 +48,55 @@ impl <T> Sub<Vector<T>> for Vector<T> where T:Add<T,Output=T>+Sub<T,Output = T>+
         Vector::new(result)
     }
 }
+//矩阵
+#[derive(Debug)]
+struct Matrix <T> where T:Add<T,Output=T>+Sub<T,Output = T>+Clone +Copy{
+    value:Vec<Vector<T>>
+}
+
+impl <T> Display for Matrix<T> where T:Add<T,Output=T>+Sub<T,Output = T>+Clone +Copy{
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let size = self.value.len();
+        let mut format_str = String::new();
+        write!(f,"Matrix {} x {}\n",size,self.value[0].size())
+//        for i in 0..size{
+//
+//            write!()
+//
+//            format_str = format_str +"";
+//
+//        }
+        //write!(f,"{}")
+    }
+}
+
+impl <T> Matrix<T> where T:Add<T,Output=T>+Sub<T,Output = T>+Clone +Copy{
+
+    fn new(list:Vec<Vector<T>>)->Matrix<T>{
+
+        Matrix{
+            value:list
+        }
+
+
+    }
+
+//    //矩阵转置
+//    fn transpose(&self)->Matrix<T>{
+//
+//    }
+
+}
 fn main(){
+
+    let mat = Matrix::new(vec![
+        Vector::new(vec![1,2,3,4]),
+        Vector::new(vec![2,3,4,5])
+    ]);
 
     let v1 = Vector::new(vec![1,2,3,4,655]);
     let v2 = Vector::new(vec![2,3,4,5,6]);
 
-    println!("{:?}",v1 - v2);
+    println!("{}",mat);
 
 }
